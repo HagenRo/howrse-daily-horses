70ffec3fed3aedd2fad89a482ca3da129f361d6e
Sharklah <berghaen@mathematik.uni-kl.de>
2025-02-27 20:44:43 +0100

    fachbereich hat anmerkungen gemacht

--- app/Database/Horse.js
+++ app/Database/Horse.js
@@ -8,16 +8,20 @@ class Horse{
     isExemption; // exception // is pluto
     exemptionFunction; // in case you are an EXEMPTION
     buttonIdentifier; // String, Klasse, ID, die den Button eindeutig identifiziert, nachdem ein Pferd keine Drop mehr durchführen kann an dem Tag. //TODO: in Konstruktorsignatur hinzufügen
+    amountRequiredClicks; // maximum amount of required clicks to finish horse; usually 1? // on some button
+    defaultType; // drop type if nothing found (Bonus for chinese) // TODO: add to constructor 
+    defaultAmount; // drop amount if nothing found (1 Bonus for chinese) // TODO: add to constructor
 
     horseLoggingObject = {
         horseURL,
         horseType,
-        horseName,
+        horseName, // intern vereinheitlichten
         dropAmount,
         dropType,
+        dropSubType, // japanese: coloured tack
         timeStamp,
         timeStampHumanReadable,
-        //...?
+        amountClicks // amount of clicks used to finish horse // together with drop amount for spice horses
     }
 
     constructor(url,searchStrings,isReadyOnWakeup,valueIfStringNotFound,isUnimportant,isExemption,exemptionFunction){
@@ -28,16 +32,15 @@ class Horse{
         this.isUnimportant = isUnimportant;
         this.isExemption = isExemption; // like Onyx with the UFOs or Pluto, with a message that doesn't trigger at one specific point in time
         this.exemptionFunction = exemptionFunction; // how to work this exemption
+        //
 
         this.horseLoggingObject.horseURL = url;
-        this.horseLoggingObject.horseType = horseType;//TODO: in methodensignatur hinzufügen
-        this.horseLoggingObject.horseName = horseName;//TODO: in methodensignatur hinzufügen
+        this.horseLoggingObject.horseType = horseType; //TODO: in methodensignatur hinzufügen
+        this.horseLoggingObject.horseName = horseName; //TODO: in methodensignatur hinzufügen
 
     }
 
     #saveHorseDropToDB(){
-        
-
         chrome.runtime.sendMessage({ function: "saveHorseDropToDB", horseLoggingObject: horseLoggingObject}, (response) => { //param1 und param2 und beliebig viele weitere können frei benannt werden, müssen dann entsprechend in backroundscript benannt sein
             //hier sind wir in der Funktion, die vom empfänger der Nachricht aufgerufen wird.
 
@@ -66,7 +69,6 @@ class Horse{
             });
         });
         if (ergebnis) {
-
             let date = new Date();
             this.horseLoggingObject.timeStamp = date.getTime();
             this.horseLoggingObject.timeStampHumanReadable = date.toISOString()
@@ -75,6 +77,8 @@ class Horse{
             //hier muss manchmal noch der typ ermittelt werden.
             //mal schauen, wie man das coden kann, dass man keine ausnamefälle betrachten muss.
             this.#saveHorseDropToDB();
+        } else {
+            // default eintrag schreiben
         }
     }
 
@@ -113,12 +117,18 @@ class Horse{
             //item = window.localStorage.getItem(this.url);
             //damit dann die berechnung machen mithilfe des reset objekts
 
+            // hochzählen einer variable, wie oft auf den button geklickt wurde
+            // nachfolgenden code nur ausführen, wenn diese variable >= mein maximum ist
+            // ? wie ist das wenn ich beim letzten draufklicken was werfe? checken, ob meine onclick methoden in sinnvoller reihenfolge ausgeführt werden 
+            // auch die anzahl an klicks speichern
+            // wenn klicks erreicht wird der leere eintrag geschrieben
+
             let date = new Date();
             this.horseLoggingObject.timeStamp = date.getTime();
             this.horseLoggingObject.timeStampHumanReadable = date.toISOString()
 
-            this.horseLoggingObject.dropAmount = 0;
-            //hier muss manchmal noch der typ ermittelt werden.
+            this.horseLoggingObject.dropAmount = 0; // TODO: nicht 0, sondern default
+            //hier muss manchmal noch der typ/subtyp ermittelt werden.
             //mal schauen, wie man das coden kann, dass man keine ausnamefälle betrachten muss.
             this.#saveHorseDropToDB();
         });
--- app/horseLogging.js
+++ app/horseLogging.js
@@ -6,8 +6,8 @@ let egypt_de = [equus_de,/.+ bringt Dir  (\d+) Fähigkeitenpunkte./,/Anubis brin
 let groom_de = [/Du hast durch Striegeln von .+ (\d+) .+ gefunden./]; // 1. Punkt: Name; 2. Punkt: Mineralien, Federn oder, in com, Schuppen // hier müsste man auf Buchstaben oder Zahlen eingrenzen
 let stellar_de = [/Du hast (\d+) x Sternstaub gefunden, indem Du den Stern der Odyssee im Weltraum enthüllst./];
 let spice_de = [/Du hast (\d+) Gewürze für Deine Fortschrittsanzeige erhalten./]; // auch exemptions?
-let jade_de = [/Du hast (\d+)  gewonnen!/];
-let japanese_de = [equus_de,/.+ bringt Dir (\d+) x  Englische Satteldecke 2** ([a-zA-Z ]+)\./]; // da gibts nen haufen möglicher gegenstände, aber es ist auch ein Link im Chat. Können wir es daraus auslesen?
+let jade_de = [/Du hast (\d+)  gewonnen\!/];
+let japanese_de = [equus_de,/.+ bringt Dir (\d+) x  Englische Satteldecke 2\*\* ([a-zA-Z ]+)\./]; // da gibts nen haufen möglicher gegenstände, aber es ist auch ein Link im Chat. Können wir es daraus auslesen?
 
 // english
 let equus_en = /.+ brings you (\d+) x  Equus./;
@@ -16,11 +16,12 @@ let spice_en = [/You have obtained (\d+) spices for your meter./];
 let spice_uk_au = [/You have obtained (\d+) spices for your metre./];
 let groom_en = [/You found (\d) .+ by grooming .+./]; // minerals or scales
 let japanese_en = [equus_en,/.+ brings you (\d+) x  Ageing point/]; // can we replace ageing point by ".+"? 
-let jade_en = [/You won (\d) !/]; // there's a passes icon in there, but should just be 1 space in text
+let jade_en = [/You won (\d) \!/]; // there's a passes icon in there, but should just be 1 space in text
 // amber drops either droppings or passes - today it was droppings, passes will be tomorrow.
 let amber_en = [/You won 10 !/,/Amber produced \d+ pounds of droppings yesterday/]; // uk and au would say "kg" instead of pounds; but this is another EXEMPTION
 let skinfaxi_au = [/Skínfaxi gave +(\d+) rays of light/]; // if that's okay
 let chinese_en = [/.+ won (\d+) skill points you can spend whichever way you like/]; // skill points watched on au
+let egypt_en = [/.+ brings you (\d+) skill points./]
 
 // exemptions from com english?
 
@@ -29,7 +30,7 @@ let stellar_nl = [/Je hebt (\d+) x Sterrenstof gevonden door de ster van de Toch
 let spice_nl = [/Je hebt (\d+) specerijen voor je meter verkregen./];
 let groom_nl = [/Je vond (\d+) stukken erts door .+ te verzorgen./]; // only metal horses if it stays like this
 let japanese_nl = [/.+ brengt je (\d+) x  Verouderingspunt./,/.+ brengt je (\d+) x  Equus./];
-let jade_nl = [/Je won (\d+) !/];
+let jade_nl = [/Je won (\d+) \!/];
 let maori_nl = [/.+ vond (\d+) x babyschildpad. Zijn of haar Tiki ontving (\d+) x mana./];
 let egypt_nl = [/Anubis brengt je (\d+) x  Leder./];
 
@@ -38,9 +39,9 @@ let egypt_nl = [/Anubis brengt je (\d+) x  Leder./];
 let stellar_se = [/Du hittade (\d+) x Stjärnstoft genom att upptäcka rymdodysséstjärnan./];
 let spice_se = [/Du har fått tag på (\d+) kryddor till din mätare./];
 let japanese_se = [/.+ ger dig (\d+) x  Åldrandepoäng./,/.+ ger dig (\d+) x  Equus./]; // APs, Eq
-let jade_se = [/Du vann (\d+) !/];
+let jade_se = [/Du vann (\d+) \!/];
 let maori_se = [/.+ hittade (\d+) x sköldpaddsunge. Deras tiki fick (\d+) x mana./];
-let egypt_se = [/.+ ger dig  (\d+) färdighetspoäng./]; // fk
+let egypt_se = [/.+ ger dig  (\d+) färdighetspoäng./,/.+ ger dig (\d+) x Järn./]; // fk
 
 
 let shenma = new Horse("https://www.howrse.de/elevage/chevaux/cheval?id=81394568",skillsChinese_de,true,"Bonus");
