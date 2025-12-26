45152371df4def04cbe180fa5cf36a70d04d39f9
Sharklah <berghaen@mathematik.uni-kl.de>
2025-12-26 22:23:19 +0100

    geklärt was angezeigt werden soll auf der extraseite

--- app/Database/Horse.js
+++ app/Database/Horse.js
@@ -244,16 +244,19 @@ class Horse{
     }
 
     #clickCounter(){
+        // TODO: Count-Wert behalten, selbst wenn die Seite neu geladen wird, bis der Drop kommt. window.localstorage.setItem
         console.log("#clickCounter ist hier");
         console.log("#clickCounter, buttonIdentifier: ",this.buttonIdentifier);
 
         $("body")[0].addEventListener('click', (event) => {
             console.log("Eventtarget: ",event.target);
-            let closestButton = $(event.target).closest("button.button.button-style-0")[0];
-            let hasCorrectParent = $(closestButton).parent(".grid-cell.odd.last")[0];
-            console.log("closestButton: ",closestButton);
-            console.log("parent: ",hasCorrectParent);
-            if (closestButton && hasCorrectParent) {
+            //let closestButton = $(event.target).closest("button.button.button-style-0")[0];
+            let isButton = $(event.target).closest(this.buttonIdentifier)[0];
+            //let hasCorrectParent = $(closestButton).parent(".grid-cell.odd.last")[0];
+            //console.log("closestButton: ",closestButton);
+            //console.log("parent: ",hasCorrectParent);
+            console.log("test: ",isButton);
+            if (isButton) {
                 console.log("horse logging object",this.horseLoggingObject);
                 this.horseLoggingObject.amountClicks+=1;
                 console.log("#clickCounter registrierte Klick Nummer ",this.horseLoggingObject.amountClicks);
--- app/dataVisualization/dataVisualization.js
+++ app/dataVisualization/dataVisualization.js
@@ -11,16 +11,28 @@ class DataParser{
 
     //TODO: hier die member anpassen, anhand der Spalten und den anzeigetabellen.
     horseDisplayObject = {
-        horseURL : null,
+        horseURL : null, // zerlegen in domain und ID
         horseType : null, // Familienname
         horseName : null, // intern vereinheitlichten
-        dropAmount : null,
-        dropType : null,
-        dropSubType : null, // japanese: coloured tack
-        timeStamp : null,
-        timeStampHumanReadable : null,
-        dropHorseAge: null,
-        amountClicks : 0, // amount of clicks used to finish horse // together with drop amount for spice horses
+        //dropAmount : null, // wird verrechnet zu Summe und Durchschnitt und auch letzter Drop Amount
+        //dropType : null, // wird verwendet um die Mengen zuzuordnen im Fall von mehreren Droptypes
+        //dropSubType : null, // japonais & sherlockAdventure: coloured tack
+        //timeStamp : null,
+        timeStampHumanReadable : null, // wird zu "letzter Drop Timestamp"
+        dropHorseAge: null, // last drop horse age
+        //amountClicks : 0, // amount of clicks used to finish horse // together with drop amount for spice horses
+
+        displayDrops : [
+            {
+                dropType : null, // inklusive subtype! // kann der gesamtdroptype sein, kann aber auch aufgesplittet nach dropmenge sein // wird (genau) bei den spices erweitert durch amountClicks
+                dropAmountSum : null,
+                dropTypeAmountAverage : null, // average über nur die Male, wo dieser Type (unabhängig der Menge) gedropt wurde
+                dropAmountAverage : null, // average über alle Drops, die Menge für diesen Type
+                lastDropAmount : null,
+                lastDropType : null,
+                countDropType : null, // anzahl, wie oft dieser Droptype gedropt wurde - zählt für den Type, und auch zusätzlich abhängig von den möglichen Mengen // bei spices auch für amountClicks
+            }
+        ]
     }
 
     horseLoggingObjects;
